1891ad3ec084a55ce4287187ad5bae5dbfa65668
danilmoiseenkov <danil.moiseenkoff@gmail.com>
2021-12-06 23:07:01 +0200

    added admin

--- currency/admin.py
+++ currency/admin.py
@@ -1,3 +1,68 @@
 from django.contrib import admin
+from currency.models import Rate, Source, ContactUs
 
-# Register your models here.
+
+class RateAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'buy',
+        'sale',
+        'created',
+        'type',
+        'source',
+    )
+
+    list_filter = (
+        'type',
+        'created',
+    )
+
+    search_fields = (
+        'buy',
+        'sale',
+        'type',
+        'source',
+    )
+
+    readonly_fields = (
+        'buy',
+        'sale',
+    )
+
+
+admin.site.register(Rate, RateAdmin)
+
+
+class SourceAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'source_url',
+        'name',
+        'phone',
+    )
+
+
+admin.site.register(Source, SourceAdmin)
+
+
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'email_from',
+        'subject',
+        'message',
+    )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+admin.site.register(ContactUs, ContactUsAdmin)
+
+
---
+++ currency/model_choices.py
@@ -0,0 +1,7 @@
+TYPE_USD = 1
+TYPE_EUR = 2
+
+TYPE_CHOICES = (
+    (TYPE_USD, 'Dollar'),
+    (TYPE_EUR, 'Euro'),
+)
