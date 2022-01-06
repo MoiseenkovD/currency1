95a9e04dc80bc94bc8c236ae26e2fbc76d44b9c3
danilmoiseenkov <danil.moiseenkoff@gmail.com>
2022-01-07 00:04:00 +0200

    task complete

---
+++ tasks.py
@@ -0,0 +1,12 @@
+def split(arr, size):
+    arrs = []
+    while len(arr) > size:
+        pice = arr[:size]
+        arrs.append(pice)
+        arr = arr[size:]
+    arrs.append(arr)
+    return arrs
+
+
+x = [1, 2, 3, 4]
+print(split(x, 2))
