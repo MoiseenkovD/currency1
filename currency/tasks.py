97bd62df79ab0f18a43c5a573c483ff9977cf2cd
danilmoiseenkov <danil.moiseenkoff@gmail.com>
2022-01-18 11:50:01 +0200

    added parsers

--- currency/tasks.py
+++ currency/tasks.py
@@ -3,6 +3,8 @@ from decimal import Decimal
 from celery import shared_task
 from currency import consts
 
+from bs4 import BeautifulSoup as bs
+
 import requests
 from currency.models import Rate, Source
 from currency import model_choices as mch
@@ -98,3 +100,74 @@ def parse_monobank():
             )
 
 
+@shared_task
+def parse_ukrsibbank():
+    r = requests.get('https://my.ukrsibbank.com/ru/personal/operations/currency_exchange/')
+    html = bs(r.content, 'html.parser')
+
+    USD_TR_INDEX = 0
+    EUR_TR_INDEX = 1
+
+    currenciesRatesTRs = html.select('.currency__table > tbody tr')
+
+    usd_buy = currenciesRatesTRs[USD_TR_INDEX].select('td')[1].contents[1]
+    usd_sale = currenciesRatesTRs[USD_TR_INDEX].select('td')[2].contents[1]
+
+    eur_buy = currenciesRatesTRs[EUR_TR_INDEX].select('td')[1].contents[1]
+    eur_sale = currenciesRatesTRs[EUR_TR_INDEX].select('td')[2].contents[1]
+
+    print('USD:', usd_buy, usd_sale)
+    print('EUR:', eur_buy, eur_sale)
+
+
+@shared_task
+def parse_minfin():
+    r = requests.get('https://minfin.com.ua/currency/')
+    html = bs(r.content, 'html.parser')
+
+    currenciesRatesDivs = html.select('.mfm-grey-bg > table > tbody > tr')
+
+    currencyUSD = currenciesRatesDivs[0].select('td')[1]
+    currencyEUR = currenciesRatesDivs[1].select('td')[1]
+
+    usd_buy = currencyUSD.contents[0].strip()
+    usd_sale = currencyUSD.contents[2].strip()
+
+    eur_buy = currencyEUR.contents[0].strip()
+    eur_sale = currencyEUR.contents[2].strip()
+
+    print(f'''USD - buy: {usd_buy}, sale: {usd_sale}''')
+
+    print(f'''EUR - buy: {eur_buy}, sale: {eur_sale}''')
+
+
+@shared_task
+def parse_kurs():
+    r = requests.get('https://kurs.com.ua/')
+    html = bs(r.content, 'html.parser')
+
+    currenciesRates = html.select('#main_table > tbody > tr')
+
+    currencyUSD_BUY = currenciesRates[0].select('td')[1]
+    currencyEUR_BUY = currenciesRates[1].select('td')[1]
+
+    currencyUSD_SAlE = currenciesRates[0].select('td')[2]
+    currencyEUR_SAlE = currenciesRates[1].select('td')[2]
+
+    print(f'''USD - buy: {currencyUSD_BUY.text}USD - sale: {currencyUSD_SAlE.text}''')
+
+    print(f'''EUR - buy: {currencyEUR_BUY.text}EUR - sale: {currencyEUR_SAlE.text}''')
+
+
+
+@shared_task
+def parse_vkurse():
+
+    url = 'http://vkurse.dp.ua/'
+
+    r = requests.get(url)
+    html = bs(r.content, 'html.parser')
+
+    for i in html.select('.container .col-xs-4.section-course'):
+        test = i.select('.pokupka-section > .pokupka-value')
+        print(test[0])
